85702b08af34c67c3b14540b25b98195bc1fc543
ritik7905 <ritik.trilok7905@gmail.com>
2023-01-05 17:55:04 +0530

    Fill-Error

--- src/pages/ourServices/OurServices.js
+++ src/pages/ourServices/OurServices.js
@@ -2,12 +2,35 @@ import React from "react";
 import { Col, Container, Row } from "react-bootstrap";
 import PageHeader from "../../components/pageHeader/PageHeader";
 import ServiceCard from "../../components/serviceCard/ServiceCard";
-import { cardData } from "./data";
+import { BsCodeSlash, BsSoundwave } from "react-icons/bs";
+import { AiOutlineHtml5 } from "react-icons/ai";
 
 // styles
 import styles from "./styles.module.scss";
 
 const OurServices = () => {
+  const cardData = [
+    {
+      cardImg: <AiOutlineHtml5 />,
+      cardTitle: "Web Design",
+      cardDetail:
+        "I value simple content structure, clean design patterns, and thoughtful interactions.",
+      techs: "HTML 5, CSS3, Bootstrap 5",
+    },
+    {
+      cardImg: <BsCodeSlash />,
+      cardTitle: "Frontend Development",
+      cardDetail: "fhjhjhjhjjkhkjhffiuytruifjkdfhdfgs",
+      techs:
+        "HTML5, CSS3, SCSS, Bootstrap 5, JavaScript (ES6+), React.Js, Next.Js, Ant.Design",
+    },
+    {
+      cardImg: <BsSoundwave />,
+      cardTitle: "Web Animation",
+      cardDetail: "fhjhjhjhjjkhkjhffiuytruifjkdfhdfgs",
+      techs: "Motion Framer, GSAP etc..",
+    },
+  ];
   return (
     <Container>
       <Row>
