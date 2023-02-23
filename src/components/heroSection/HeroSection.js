c7e9d39f1a5fd00739a1ce868e477a8c155383ec
ritik7905 <ritik.trilok7905@gmail.com>
2023-02-23 17:24:14 +0530

    Add: Removed space from letters

--- src/components/heroSection/HeroSection.js
+++ src/components/heroSection/HeroSection.js
@@ -14,7 +14,7 @@ const HeroSection = () => {
           <h2 className={styles.name}>Ritik Sharma</h2>
           <h3 className={styles.quotes}>I build things for the web.</h3>
           <p className={styles.desc}>
-            I am a Frontend engineer.I work in tech as a
+            I am a Frontend engineer. I work in tech as a
             <strong> UI Developer</strong>. My responsibilities to build
             re-usable UI components and pixel-perfect design. I collaborate with
             my team. Excited to be part of this innovative industry and
