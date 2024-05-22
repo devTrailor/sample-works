f6e289ac547fafa40e7ec2e741f073d2024d822b
ritikTechchefz <ritik.sharma@techchefz.com>
2024-05-22 15:49:54 +0530

    WIP

--- src/components/navbar/Navigation.js
+++ src/components/navbar/Navigation.js
@@ -6,6 +6,7 @@ import Logo from "../logo/Logo";
 // Styles
 import styles from "./styles.module.scss";
 const Navigation = () => {
+
   const navigation = [
     {
       name: "Home",
@@ -32,24 +33,32 @@ const Navigation = () => {
       path: "#contact",
     },
   ];
+
   return (
-    <Navbar expand="lg" className={styles.custom_nav}>
+    <nav expand="lg" className={styles.custom_nav}>
       <Container>
-        <Link href="#home" className={styles.nav_logo}>
-          {/* Logo */}
-          <Logo />
-        </Link>
-        <ul className={styles.nav_links}>
-          {navigation.map((elm) => {
-            return (
-              <li className={styles.links} key={elm.path}>
-                <Link href={elm.path}>{elm.name}</Link>
-              </li>
-            );
-          })}
-        </ul>
+        <div className={styles.inner}>
+          <Link href="#home" className={styles.nav_logo}>
+            {/* Logo */}
+            <Logo />
+          </Link>
+          <ul className={styles.nav_links}>
+            {navigation.map((elm) => {
+              return (
+                <li className={styles.links} key={elm.path}>
+                  <Link href={elm.path}>{elm.name}</Link>
+                </li>
+              );
+            })}
+          </ul>
+
+          <div className={styles.actionMenu}>
+            Icon Menu
+            Icon Close
+          </div>
+        </div>
       </Container>
-    </Navbar>
+    </nav>
   );
 };
 
