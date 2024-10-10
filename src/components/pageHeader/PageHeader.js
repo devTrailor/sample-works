3a1a20f6724ad01eb1561de71a18be6960271b83
ritikTechchefz <ritik.sharma@techchefz.com>
2024-10-10 18:26:45 +0530

    ADD: WIP

--- src/components/pageHeader/PageHeader.js
+++ src/components/pageHeader/PageHeader.js
@@ -1,15 +1,34 @@
 import React from "react";
+import { motion } from "framer-motion";
 
 // styles
 import styles from "./styles.module.scss";
 const PageHeader = (props) => {
   const { mainTitle, num } = props;
+
+  const pageHeaderVariants = {
+    hidden: {
+      opacity: 0,
+      x: '-100%'
+    },
+    visible: {
+      opacity: [0.5, 1],
+      x: 0,
+    }
+  };
+
   return (
-    <div className={`${styles.pageHeader}`}>
+    <motion.div
+      className={`${styles.pageHeader}`}
+      variants={pageHeaderVariants}
+      initial="hidden"
+      animate="visible"
+      transition={{ duration: 1.5, ease: 'backInOut' }}
+    >
       <span className={styles.num}>{num}</span>
       <h2 className={styles.mainTitle}>{mainTitle}</h2>
       <span className={styles.line}></span>
-    </div>
+    </motion.div>
   );
 };
 
--- src/layout/LayoutPrimary.js
+++ src/layout/LayoutPrimary.js
@@ -1,3 +1,4 @@
+import { motion } from "framer-motion";
 import Footer from "../components/footer/Footer";
 import Navigation from "../components/navbar/Navigation";
 
@@ -5,11 +6,27 @@ import Navigation from "../components/navbar/Navigation";
 import styles from "./styles.module.scss";
 
 const LayoutPrimary = ({ children }) => {
+  const headerVariants = {
+    hidden: {
+      opacity: 0,
+      y: -100,
+    },
+    visible: {
+      opacity: 1,
+      y: 0,
+    }
+  };
   return (
     <section className={styles.main_layout}>
-      <section className={styles.navigation_sec}>
+      <motion.section
+        className={styles.navigation_sec}
+        variants={headerVariants}
+        initial="hidden"
+        animate="visible"
+        transition={{ duration: 0.5, ease: 'backInOut' }}
+      >
         <Navigation />
-      </section>
+      </motion.section>
       {children}
       <section className={styles.footerSec}>
         <Footer />
